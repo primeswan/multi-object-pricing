a5ac8407d7c5bb181f16e207d1689133fbdc42a1
Paramahamsa Kolagani <141573684+primeswan@users.noreply.github.com>
2024-01-30 17:02:33 -0500

    Add files via upload

---
+++ project/config.py
@@ -0,0 +1,22 @@
+
+## Parameters for the additive model with 2 items.
+
+# Model parameters
+num_features = 2
+hidden_neurons = 100
+input_shape = (num_features,)
+softmax_multiplier = 1000
+
+# Training parameters
+batch_size = 10000
+learning_rate = 0.001
+train_size = 1000000
+num_epochs = 100
+
+# Testing parameters
+test_size = 50000
+# Threshold for getting optimal menu
+threshold = 0.1
+
+
+
